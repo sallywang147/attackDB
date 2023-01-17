d66c1d9735c54453b7e0ccb346b972d87e11d217
Sally <jw4074@columbia.edu>
2023-01-17 00:30:22 -0500

    add one attack

---
+++ noodleattack/contrived.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+// SushiBar is the coolest bar in town. You come in with some Sushi, and leave with more! The longer you stay, the more Sushi you get.
+//
+// This contract handles swapping to and from xSushi, SushiSwap's staking token.
+contract SushiBar is ERC20("SushiBar", "xSUSHI"){
+    using SafeMath for uint256;
+    IERC20 public sushi;
+
+    // Define the Sushi token contract
+    constructor(IERC20 _sushi) public {
+        sushi = _sushi;
+    }
+
+    // Enter the bar. Pay some SUSHIs. Earn some shares.
+    // Locks Sushi and mints xSushi
+    function enter(uint256 _amount) public {
+        // Gets the amount of Sushi locked in the contract
+        uint256 totalSushi = sushi.balanceOf(address(this));
+        // Gets the amount of xSushi in existence
+        uint256 totalShares = totalSupply();
+        // If no xSushi exists, mint it 1:1 to the amount put in
+        if (totalShares == 0 || totalSushi == 0) {
+            _mint(msg.sender, _amount);
+        } 
+        // Calculate and mint the amount of xSushi the Sushi is worth. The ratio will change overtime, as xSushi is burned/minted and Sushi deposited + gained from fees / withdrawn.
+        else {
+            uint256 what = _amount.mul(totalShares).div(totalSushi);
+            //bug!! this is where reentrent happens: 
+            _mint(msg.sender, what);
+        }
+        // Lock the Sushi in the contract
+        sushi.transferFrom(msg.sender, address(this), _amount);
+    }
+}
---
+++ noodleattack/healthy-contracts/README.md
@@ -0,0 +1,20 @@
+Below is the official annnouncement from n00dle:  the fix is to employ new tokens
+
+- Deploy new ERC20 tokens (these new tokens cannot interact with previous contracts, hence providing extra safety)
+- Snapshot the current 3gg and n00d holders
+- Create a merkle claim contract to allow holders claim the new tokens
+- Resume liquidity on new tokens
+
+Please make any suggestions in #💬・lounge regarding the solution.
+≈ Chef Mee ≈ — 11/04/2022 3:24 PM
+@everyone within the next few hours, we will create new tokens and airdrop to existing token (n00d and 3gg) holders. Holders would not need to do anything. 
+
+Rationale behind why we need to deploy a new token
+- The exploited contracts will haunt us forever had we not deployed a new token
+
+Rationale behind why we are not deploying a token swap contract but snapshotting the current token holders
+- We have no control over when the token swap will end had we deployed a token swap contract. It might drag on forever. Airdropping is the quickest way to help us resume liquidity
+- Snapshotting and airdropping is a more gas and engineering efficient method considering the amount of holders
+
+We will conduct the snapshot at 4:00pm ET. Please refrain from transferring the tokens after 3:30pm ET.
+We will immediately notify etherscan and coingecko after the airdrop
