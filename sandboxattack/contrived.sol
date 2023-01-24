0fe196880cf81905dfbbd62e99a97595a3e82836
Sally <jw4074@columbia.edu>
2023-01-23 23:01:15 -0500

    a

---
+++ sandboxattack/contrived.sol
@@ -0,0 +1,11 @@
+/* solhint-disable func-order, code-complexity */
+pragma solidity 0.5.9;
+
+contract contrived {
+    function _burn(address from, address owner, uint256 id) public {
+        require(from == owner, "not owner");
+        _owners[id] = 2**160; // cannot mint it again
+        _numNFTPerAddress[from]--;
+        emit Transfer(from, address(0), id);
+    }
+}
