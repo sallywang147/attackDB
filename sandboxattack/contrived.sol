ac5449f9bc3e22466f37dc406a336e012f60d21e
Sally <jw4074@columbia.edu>
2023-01-23 23:07:09 -0500

    commit

--- sandboxattack/contrived.sol
+++ sandboxattack/contrived.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.5.9;
 
 contract contrived {
+    //this should be interal 
     function _burn(address from, address owner, uint256 id) public {
         require(from == owner, "not owner");
         _owners[id] = 2**160; // cannot mint it again
